84d733c9ee36cebf2d63dbc6db9446d856ff5371
Linh Nghiem <l.nghiemum@gmail.com>
2024-05-27 20:05:53 +1000

    Add files via upload
    fixing cell_weights_matrix2 to ensure the weight is semi positive definite

--- R/cells_weight_matrix2.R
+++ R/cells_weight_matrix2.R
@@ -5,18 +5,15 @@
 #' @description
 #' This function says hello
 #'
-#' @param coords no arguments
-#' @param labels to fill
-#' @param alpha to fill
+#' @param none no arguments
 #'
 #' @return prints hello world
 #'
 #' @examples
-#' #hello()
+#' hello()
 #'
 #' @export
 cells_weight_matrix2 <- function(coords, labels, alpha = 1) {
-
   avg_coords <- slicer_categorical(coords, labels)
 
   avg_coords_groups <- gsub(".*, ", "", rownames(avg_coords))
@@ -29,10 +26,17 @@ cells_weight_matrix2 <- function(coords, labels, alpha = 1) {
 
   dist_mat_new = dist_mat + D2
 
-  dist_norm <- 1 - dist_mat_new / max(dist_mat, na.rm = TRUE)
-
-  weight_mat <- (dist_norm^alpha * 2) - 1
-
+  dist_norm <- 1 - (dist_mat_new / max(dist_mat, na.rm = TRUE))
+  
+  weight_mat <- (dist_norm^alpha* 2) - 1
+  # ensure it is psd
+  eig <- eigen(weight_mat) 
+  k <- eig$values > 1e-8
+  weight_mat <- eig$vectors[, k, drop = F] %*% 
+    diag(eig$values[k]) %*% 
+    t(eig$vectors[, k, drop = F])
+  
+  
   weight_mat[!is.finite(weight_mat)] <- 0 #(-1)
 
   return(weight_mat)
