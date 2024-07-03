0ca272ddea7427407b3cfbe5291c1d8a643f2cd4
Mwoo-usyd <106008614+Mwoo-usyd@users.noreply.github.com>
2024-07-03 12:12:37 +1000

    Update slicer_categorical.R
    complete documentation

--- R/slicer_categorical.R
+++ R/slicer_categorical.R
@@ -1,19 +1,15 @@
-# Hello world function
-
-#' Day hello
+#' slicer_categorical
 #'
 #' @description
-#' This function says hello
+#' This function averages all columns in the X matrix for each category in the single-column Y dataframe. It is used to find the average position within each tile to create the weight matrix, as well as to create the tile means that are used in the eigendecomposition step of SIR/wSIR.
 #'
-#' @param X no arguments
-#' @param Y to fill
+#' @param X matrix or dataframe containing the columns to average
+#' @param Y dataframe with a single column named 'coordinate'. This column contains the categorical variable tile ID for each cell. 
 #'
-#' @return prints hello world
+#' @return dataframe containing the averages for each column within each category in Y. Dimension h * p, where h is the number of categories in Y and p is the number of columns in X.
 #'
-#' @examples
-#' #hello()
-#'
-#' @export
+#' @keywords internal
+
 slicer_categorical <- function(X, Y) {
 
   # this function specifically slices with categorical Y
