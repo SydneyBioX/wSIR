c0379623ba85c7f26b3f3818d99af2f4ba548f1f
Mwoo-usyd <106008614+Mwoo-usyd@users.noreply.github.com>
2024-07-05 00:21:52 +1000

    Update project_wSIR.R
    State new gene expr data must have same columns as old gene expr data

--- R/project_wSIR.R
+++ R/project_wSIR.R
@@ -5,7 +5,8 @@
 #'
 #' @param wsir wsir object that is usually the output of wSIR function. If you want to project new data into low-dim space following a 
 #' different DR method, at param wsir use a list with loadings in slot 2 (e.g PCA loadings) of dimension p * d
-#' @param newdata matrix of new gene expression data to project into low-dimensional space
+#' @param newdata matrix of new gene expression data to project into low-dimensional space. Must have the same p columns
+#' as the columns in X argument used to generate wsir. 
 #'
 #' @return matrix of low-dimensional representation of newdata gene expression data
 #'
