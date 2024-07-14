5ea0a50643e9435ea9c106b16bfab588beabbb78
Max Woollard <maxwoollard@192-168-1-14.tpgi.com.au>
2024-07-14 22:18:17 +1000

    Include rownames for WSIR directions ( = gene names) and fix bug to optimise wsir parameters a second time

--- R/sir_categorical.R
+++ R/sir_categorical.R
@@ -3,10 +3,10 @@
 #' @description
 #' This function performs WSIR based on provided gene expression data, tile allocations, and weight matrix.
 #'
-#' @param X matrix of normalised gene expression data for n genes across p cells. 
+#' @param X matrix of normalised gene expression data for n genes across p cells.
 #' @param Y dataframe with 1 column named "coordinate" that is the tile allocation for each cell. There should be up to slices^2 unique tile IDs in this column.
 #' @param directions Integer to specify maximum number of directions to retain in thee low-dimensional embedding of the data. Use if you need at most a certain number for a downstream task.
-#' @param W Weight matrix created by cells_weight_matrix. Entry (i,j) represents the spatial correlation level between tiles i and j. The diagonal values should be all 1. If not provided, SIR implementation will be used. 
+#' @param W Weight matrix created by cells_weight_matrix. Entry (i,j) represents the spatial correlation level between tiles i and j. The diagonal values should be all 1. If not provided, SIR implementation will be used.
 #' @param varThreshold numeric value specifying the desired proportion of variance to retain. If e.g 95% (default), then number of directions used will be such that their eigenvalues add up to 95% of the sum of all eigenvalues.
 #'
 #' @return list of outputs witth 5 named slots. They are the same as the output of the wSIR function: this is the final step in the wSIR function.
@@ -36,6 +36,7 @@ sir_categorical <- function(X, Y, directions = 50, W = NULL, varThreshold = 0.95
   betas <- apply(betas, 2, function(x) x/sqrt(sum(x^2)))
   final_XB <- as.matrix(X) %*% betas
 
+  rownames(betas) <- colnames(X)
 
   return(list(scores = final_XB,
               directions = betas,
